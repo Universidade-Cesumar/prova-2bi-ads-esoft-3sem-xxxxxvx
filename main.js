ba0ed50b2dd79d21bafc1cecbdb98b366f3a7413
giulia <lianegreli@gmail.com>
2026-06-10 19:52:27 -0300

    configura o main.js com a url da mockapi(de materiais) e criação de funções

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-// Arquivo para código javascript
+'use strict';
+
+const URL_MATERIAIS = 'https://6a29e850f59cb8f65f1dc0f8.mockapi.io/materiais';
+
+const dataAtual = () => {
+    const hoje = new Date();
+    const dia = String(hoje.getDate()).padStart(2, '0');
+    const mes = String(hoje.getMonth() + 1).padStart(2, '0');
+    const ano = hoje.getFullYear();
+    return `${dia}/${mes}/${ano}`;
+}
+
+const limparFormulario = () => {
+    document.getElementById('input-nome').value = '';
+    document.getElementById('input-quantidade').value = '';
+    document.getElementById('input-unidade').value = '';
+    document.getElementById('input-observacoes').value = '';
+}
